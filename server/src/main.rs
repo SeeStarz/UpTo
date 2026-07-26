df4d6e995407abc86267b7d50d856b2358a8ab8d
SeeStarz <mfahrim7@gmail.com>
2026-07-26 23:18:58 +0700

    Add frontend

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,23 +1,38 @@
 use axum::{
     Json, Router,
+    extract::State,
     routing::{get, post},
 };
 use common::Snapshot;
-use std::println;
+use std::{println, sync::Arc};
+use tokio::sync::Mutex;
+use tower_http::cors::CorsLayer;
+
+type AppState = Arc<Mutex<Option<Snapshot>>>;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
+    let state: AppState = Arc::new(Mutex::new(None));
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/api", post(post_api));
+        .route("/api", post(post_api).get(get_api))
+        .layer(CorsLayer::permissive())
+        .with_state(state);
 
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
-    println!("Hello");
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn post_api(Json(payload): Json<Snapshot>) {
-    eprintln!("{:?}", payload);
+async fn post_api(State(state): State<AppState>, Json(payload): Json<Snapshot>) {
+    println!("{:?}", payload);
+
+    let mut snapshot = state.lock().await;
+    *snapshot = Some(payload);
+}
+
+async fn get_api(State(state): State<AppState>) -> Json<Option<Snapshot>> {
+    let snapshot = state.lock().await;
+
+    Json(snapshot.clone())
 }
