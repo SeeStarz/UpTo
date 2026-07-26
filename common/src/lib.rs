7f6676f7aa8c0ba54be6dcc8b0e64d6f7c9f79a4
SeeStarz <mfahrim7@gmail.com>
2026-07-26 18:24:22 +0700

    Initial commit

---
+++ common/src/lib.rs
@@ -0,0 +1,9 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct Fact {
+    pub observer_name: String,
+    pub observation_title: String,
+    pub metadata: HashMap<String, String>,
+}
